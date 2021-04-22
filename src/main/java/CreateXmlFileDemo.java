ea66e00d7a1a33fb5ecb71ecfc05876faee1cb25
Diminet <70798333+Diminet@users.noreply.github.com>
2021-04-22 23:08:29 +0300

    Update CreateXmlFileDemo.java

--- src/main/java/CreateXmlFileDemo.java
+++ src/main/java/CreateXmlFileDemo.java
@@ -18,164 +18,173 @@ class XMLCreate {
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.newDocument();
 
-        
+            // books (root) element
             Element films = doc.createElement("films");
             doc.appendChild(films);
 
+            // genre1 element
             Element Category = doc.createElement("Category");
-            booksElement.appendChild(Category);
+            films.appendChild(Category);
 
-        
+            // setting genreID attribute to element genre1
             Attr idCategory = doc.createAttribute("idCategory");
             idCategory.setValue("001");
             Category.setAttributeNode(idCategory);
 
+            // setting genreName attribute to element genre1
             Attr Categoryname = doc.createAttribute("Categoryname");
-            genreName.setValue("history");
-            genre1.setAttributeNode(genreName);
+            Categoryname.setValue("history");
+            Category.setAttributeNode(Categoryname);
 
-   
-            Element book1 = doc.createElement("film");
-            genre1.appendChild(book1);
+            // book1 element
+            Element film1 = doc.createElement("film");
+            Category.appendChild(film1);
 
-           
-            Attr bookId = doc.createAttribute("filmid");
-            bookId.setValue("0001");
-            book1.setAttributeNode(bookId);
+            // setting bookId attribute to element book
+            Attr filmid = doc.createAttribute("filmid");
+            filmid.setValue("0001");
+            film1.setAttributeNode(filmid);
 
-          
-            Element bookAuthor = doc.createElement("Regizor");
-            bookAuthor.appendChild(doc.createTextNode("Simon Stone"));
-            book1.appendChild(bookAuthor);
+            // author element
+            Element Regizor = doc.createElement("Regizor");
+            Regizor.appendChild(doc.createTextNode("Simon Stone"));
+            film1.appendChild(Regizor);
 
-            Element bookName = doc.createElement("Name");
-            bookName.appendChild(doc.createTextNode("The Dig"));
-            book1.appendChild(bookName);
+            // book name element
+            Element Name = doc.createElement("Name");
+            Name.appendChild(doc.createTextNode("The Dig"));
+            film1.appendChild(Name);
 
-            Element publicationDate = doc.createElement("Publication");
-            publicationDate.appendChild(doc.createTextNode("2222"));
-            book1.appendChild(publicationDate);
+            // publication date element
+            Element Publication = doc.createElement("Publication");
+            Publication.appendChild(doc.createTextNode("2222"));
+            film1.appendChild(Publication);
 
-        
+            // availability element
             Element availability = doc.createElement("availability");
             availability.appendChild(doc.createTextNode("true"));
-            book1.appendChild(availability);
+            film1.appendChild(availability);
 
-            Element characters = doc.createElement("Casts");
-            book1.appendChild(characters);
+            // characters list element
+            Element Casts = doc.createElement("Casts");
+            film1.appendChild(Casts);
 
-            Element character1 = doc.createElement("Cast");
-            character1.appendChild(doc.createTextNode("Lily James"));
-            characters.appendChild(character1);
+            // character elements
+            Element Cast1 = doc.createElement("Cast");
+            Cast1.appendChild(doc.createTextNode("Lily James"));
+            Casts.appendChild(Cast1);
 
-            Element character2 = doc.createElement("Cast");
-            character2.appendChild(doc.createTextNode("Ralph Fiennes"));
-            characters.appendChild(character2);
+            Element Cast2 = doc.createElement("Cast");
+            Cast2.appendChild(doc.createTextNode("Ralph Fiennes"));
+            Casts.appendChild(Cast2);
 
-            Element character3 = doc.createElement("Cast");
-            character3.appendChild(doc.createTextNode("Carey Mulligan"));
-            characters.appendChild(character3);
+            Element Cast3 = doc.createElement("Cast");
+            Cast3.appendChild(doc.createTextNode("Carey Mulligan"));
+            Casts.appendChild(Cast3);
 
 
 
-       
-            Element book2 = doc.createElement("film");
-            genre1.appendChild(book2);
 
-          
-            Attr book2Id = doc.createAttribute("filmid");
-            book2Id.setValue("0002");
-            book2.setAttributeNode(book2Id);
+            Element film2 = doc.createElement("film");
+            Category.appendChild(film2);
 
-         
-            Element book2Author = doc.createElement("Regizor");
-            book2Author.appendChild(doc.createTextNode("Thomas Kail"));
-            book2.appendChild(book2Author);
 
-            Element book2Name = doc.createElement("Name");
-            book2Name.appendChild(doc.createTextNode("Hamilton"));
-            book2.appendChild(book2Name);
+            Attr filmid2 = doc.createAttribute("filmid");
+            filmid2.setValue("0002");
+            film2.setAttributeNode(filmid2);
 
-            Element publicationDate2 = doc.createElement("Publication");
-            publicationDate2.appendChild(doc.createTextNode("2020"));
-            book2.appendChild(publicationDate2);
 
+            Element Regizor2 = doc.createElement("Regizor");
+            Regizor2.appendChild(doc.createTextNode("Thomas Kail"));
+            film2.appendChild(Regizor2);
+
+            // book name element
+            Element Name2 = doc.createElement("Name");
+            Name2.appendChild(doc.createTextNode("Hamilton"));
+            film2.appendChild(Name2);
+
+            // publication date element
+            Element Publication2 = doc.createElement("Publication");
+            Publication2.appendChild(doc.createTextNode("2020"));
+            film2.appendChild(Publication2);
+
+            // availability element
             Element availability2 = doc.createElement("availability");
             availability2.appendChild(doc.createTextNode("true"));
-            book2.appendChild(availability2);
+            film2.appendChild(availability2);
 
-            Element characters2 = doc.createElement("Casts");
-            book2.appendChild(characters2);
+            // characters list element
+            Element Casts2 = doc.createElement("Casts");
+            film2.appendChild(Casts2);
 
-      
-            Element character12 = doc.createElement("Cast");
-            character12.appendChild(doc.createTextNode("Brego"));
-            characters2.appendChild(character12);
+            // character elements
+            Element Cast12 = doc.createElement("Cast");
+            Cast12.appendChild(doc.createTextNode("Brego"));
+            Casts2.appendChild(Cast12);
 
-            Element character22 = doc.createElement("Cast");
-            character22.appendChild(doc.createTextNode("Daveed Diggs"));
-            characters2.appendChild(character22);
+            Element Cast22 = doc.createElement("Cast");
+            Cast22.appendChild(doc.createTextNode("Daveed Diggs"));
+            Casts2.appendChild(Cast22);
 
-            Element character32 = doc.createElement("Cast");
-            character32.appendChild(doc.createTextNode("Lin‑Manuel Miranda"));
-            characters2.appendChild(character32);
+            Element Cast32 = doc.createElement("Cast");
+            Cast32.appendChild(doc.createTextNode("Lin‑Manuel Miranda"));
+            Casts2.appendChild(Cast32);
 
 
 
-            Element genre2 = doc.createElement("Category");
-            booksElement.appendChild(genre2);
 
-            Attr genre2Id = doc.createAttribute("idCategory");
-            genre2Id.setValue("002");
-            genre2.setAttributeNode(genre2Id);
+            Element Category2 = doc.createElement("Category");
+            films.appendChild(Category2);
 
-            Attr genre2Name = doc.createAttribute("Categoryname");
-            genre2Name.setValue("Drama");
-            genre2.setAttributeNode(genre2Name);
+            Attr idCategory2 = doc.createAttribute("idCategory");
+            idCategory2.setValue("002");
+            Category2.setAttributeNode(idCategory2);
 
-    
-            Element book3 = doc.createElement("film");
-            genre2.appendChild(book3);
+            Attr Categoryname2 = doc.createAttribute("Categoryname");
+            Categoryname2.setValue("Drama");
+            Category2.setAttributeNode(Categoryname2);
 
-        
-            Attr book3Id = doc.createAttribute("filmid");
-            book3Id.setValue("0003");
-            book3.setAttributeNode(book3Id);
+            Element film3 = doc.createElement("film");
+            Category2.appendChild(film3);
 
-      
-            Element book3Author = doc.createElement("Regizor");
-            book3Author.appendChild(doc.createTextNode("Aaron Sorkin"));
-            book3.appendChild(book3Author);
+            Attr filmid3 = doc.createAttribute("filmid");
+            filmid3.setValue("0003");
+            film3.setAttributeNode(filmid3);
 
-            Element book3Name = doc.createElement("Name");
-            book3Name.appendChild(doc.createTextNode("The Trial of the Chicago 7"));
-            book3.appendChild(book3Name);
+            Element Regizor3 = doc.createElement("Regizor");
+            Regizor3.appendChild(doc.createTextNode("Aaron Sorkin"));
+            film3.appendChild(Regizor3);
 
-    
-            Element publicationDate3 = doc.createElement("Publication");
-            publicationDate3.appendChild(doc.createTextNode("2020"));
-            book3.appendChild(publicationDate3);
 
-     
+            Element Name3 = doc.createElement("Name");
+            Name3.appendChild(doc.createTextNode("The Trial of the Chicago 7"));
+            film3.appendChild(Name3);
+
+
+            Element Publication3 = doc.createElement("Publication");
+            Publication3.appendChild(doc.createTextNode("2020"));
+            film3.appendChild(Publication3);
+
+
             Element availability3 = doc.createElement("availability");
             availability3.appendChild(doc.createTextNode("true"));
-            book3.appendChild(availability3);
+            film3.appendChild(availability3);
 
-            Element characters3 = doc.createElement("Casts");
-            book3.appendChild(characters3);
+            Element Casts3 = doc.createElement("Casts");
+            film3.appendChild(Casts3);
 
-       
-            Element character13 = doc.createElement("Cast");
-            character13.appendChild(doc.createTextNode("Eddie Redmayne"));
-            characters3.appendChild(character13);
+            Element Cast31 = doc.createElement("Cast");
+            Cast31.appendChild(doc.createTextNode("Eddie Redmayne"));
+            Casts3.appendChild(Cast31);
 
-            Element character23 = doc.createElement("Cast");
-            character23.appendChild(doc.createTextNode("Sacha Baron Cohen"));
-            characters3.appendChild(character23);
+            Element Cast322 = doc.createElement("Cast");
+            Cast322.appendChild(doc.createTextNode("Sacha Baron Cohen"));
+            Casts3.appendChild(Cast322);
 
-            Element character33 = doc.createElement("Cast");
-            character33.appendChild(doc.createTextNode("Jeremy Strong"));
-            characters3.appendChild(character33);
+            Element Cast323 = doc.createElement("Cast");
+            Cast323.appendChild(doc.createTextNode("Jeremy Strong"));
+            Casts3.appendChild(Cast323);
+
 
 
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
@@ -184,7 +193,7 @@ class XMLCreate {
             StreamResult result = new StreamResult(new File("film.xml"));
             transformer.transform(source, result);
 
-       
+
             StreamResult consoleResult = new StreamResult(System.out);
             transformer.transform(source, consoleResult);
         } catch (Exception e) {
